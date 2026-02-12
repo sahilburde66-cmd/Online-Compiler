caa43f316c96f5f59275f73d606c858aa46519ae
anchal-shukla123 <anchalshukla423@gmail.com>
2026-02-12 12:31:28 +0530

    path added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ErrorBoundary from "../components/ErrorBoundary";
 import Adsense from "../components/Adsense";
+
 // Lazy load pages for better performance
 const Home = lazy(() => import("../pages/Home"));
 const AboutUs = lazy(() => import("../pages/AboutUs"));
