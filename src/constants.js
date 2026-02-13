43b6a9797ab7239d34b5e24ae0dd5492251dfc78
anchal-shukla123 <anchalshukla423@gmail.com>
2026-02-13 11:58:08 +0530

    c and c++ added

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,8 @@ export const LANGUAGE_VERSIONS = {
   java: "15.0.2",
   csharp: "6.12.0",
   php: "8.2.3",
+  cpp: "10.2.0",
+  c: "10.2.0",
 };
 
 export const CODE_SNIPPETS = {
@@ -15,4 +17,6 @@ export const CODE_SNIPPETS = {
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Alex';\necho $name;\n",
+  cpp: `#include <iostream>\nusing namespace std;\n\nint main() {\n\tstring name = "Alex";\n\tcout << "Hello, " << name << "!" << endl;\n\treturn 0;\n}\n`,
+  c: `#include <stdio.h>\n\nint main() {\n\tchar name[] = "Alex";\n\tprintf("Hello, %s!\\n", name);\n\treturn 0;\n}\n`,
 };
